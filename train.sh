d0a342f2374a1a0bc9ea80c2ef7f6daa85a20c28
Aleksey Korshuk <ale-kor02@mail.ru>
2023-04-14 19:58:43 -0700

    updates

--- train.sh
+++ train.sh
@@ -1,17 +1,22 @@
 deepspeed run_clm.py \
-  --model_name_or_path gpt2 \
+  --model_name_or_path EleutherAI/pythia-1.4b-deduped \
   --dataset_name Fraser/short-jokes \
   --per_device_train_batch_size 32 \
   --per_device_eval_batch_size 32 \
+  --gradient_accumulation_steps 1 \
   --do_train \
   --do_eval \
   --output_dir /tmp/test-clm \
   --report_to wandb \
   --num_train_epochs 1 \
   --push_to_hub \
-  --hub_model_id AlekseyKorshuk/gpt2-jokes \
+  --hub_model_id AlekseyKorshuk/pythia-1.4b-deduped-jokes \
   --overwrite_output_dir \
   --evaluation_strategy "steps" \
   --eval_steps 100 \
   --save_strategy epoch \
-  --save_steps 1
+  --save_steps 1 \
+  --learning_rate 5e-5 \
+  --lr_scheduler_type "cosine" \
+  --warmup_ratio 0.05
+
